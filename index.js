b36ad176a505f8aa659070373b89db1245c19507
Samnan <samnansiddique@yandex.com>
2023-05-13 19:58:18 +0530

    Minor changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => {
-    res.send({ message: "Backend Test © ANUPAMA ❤️ SAMNAN" });
+    res.send({ message: "Backend Test" });
 });
 
 app.use("/api/v1/users", userRouter);
